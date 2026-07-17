95b80f4b4311fd4e3e69200e6acb6c4932ed2345
KEX-03 <killex003@outlook.com>
2026-07-17 11:12:31 +0530

    import and use `ueLocalStorage()` in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
+import useLocalStorage from './useLocalStorage'
 
 function App() {
-  const [name, setName] = useState('')
+  const [name, setName] = useLocalStorage('')
 
   return (
     <input 
